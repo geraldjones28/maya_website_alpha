91eb9d0cfe268f3cdb5f0580e85c5bc6b4286641
GeraldJones <gajone12@asu.edu>
2024-02-27 17:00:43 -0800

    feat: Move all Scripting into separate Javascript file

--- javascript.js
+++ javascript.js
@@ -2,3 +2,9 @@ console.log("Here is a hidden message. shhhh");
 
 let message = "You're the best wife ever!";
 alert(message);
+
+function buyBook() {
+    alert("When they click this, a link to buy your book will pop up!");}
+
+function signUp() {
+    alert("When they click this, they will be able to input their email address to get notifications for your future podcast/book releases!")}
